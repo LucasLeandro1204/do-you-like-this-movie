19152db627669d434b818a4bc389bba788e754bc
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-01-31 22:43:54 -0200

    wtf

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './app/App.vue';
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+new Vue(App).$mount('#app');
