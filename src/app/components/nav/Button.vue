d4e4a4770ef317179b6082d5554dca1ef883b629
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-02-04 21:25:18 -0200

    button nav

---
+++ src/app/components/nav/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click.prevent="toggle">
+    <svg class="fill-current text-grey-darker" width="32" height="24" viewBox="0 0 32 24" preserveAspectRatio="xMidYMid meet">
+      <rect width="32" height="4" rx="5" ry="5" />
+      <rect y="8" width="24" height="4" rx="5" ry="5" />
+      <rect y="16" width="16" height="4" rx="5" ry="5" />
+    </svg>
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: Boolean,
+    },
+
+    methods: {
+      toggle () {
+        const open = ! this.value;
+
+        this.$emit('toggle', open);
+        this.$emit('input', open);
+      },
+    },
+  };
+</script>
