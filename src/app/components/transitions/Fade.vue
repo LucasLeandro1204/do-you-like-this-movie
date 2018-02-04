c931ce0b362fb970e28f93a71915906f43654202
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-02-04 21:30:12 -0200

    fade component

---
+++ src/app/components/transitions/Fade.vue
@@ -0,0 +1,17 @@
+<template>
+  <transition name="fade">
+    <slot></slot>
+  </transition>
+</template>
+
+<style scoped>
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity .3s;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
