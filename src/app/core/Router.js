b077a43905a6fbabecdebfa7dbc3ce03251f89e7
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-02-06 22:11:09 -0200

    index component

--- src/app/core/Router.js
+++ src/app/core/Router.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import Index from 'app/Index.vue';
+
 Vue.use(VueRouter);
 
 const routes = [
-
+  { path: '/', name: 'index', component: Index },
 ];
 
 export default new VueRouter({
